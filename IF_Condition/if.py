713e2a68d89ef721ef07197b9c8e7db1fc73a21b
bmpatel15 <bpatel501@gmail.com>
2025-03-01 14:54:56 -0500

    this is my fourth commit

---
+++ IF_Condition/if.py
@@ -0,0 +1,9 @@
+"""I am practicing my IF conditions"""
+
+n = int(input("Enter a number: "))
+print(n)
+
+if n % 2 == 0:
+    print(f"{n} is an even number")
+else:
+    print(f"{n} is an odd number")
--- mypython.py
+++ mypython.py
@@ -1,2 +1,3 @@
-print('Hello World!!!!')
+"""This is a sample Python script"""
 
+print("Hello World!!!!")
