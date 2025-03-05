e5b30dc2846b3970a9f466991e3ac1ce29678aad
bmpatel15 <bpatel501@gmail.com>
2025-03-04 23:41:56 -0500

    made some updates to my learning

---
+++ ForLoop/forloop.py
@@ -0,0 +1,1 @@
+# This is a test file so that i can learn how to do for loops
--- IF_Condition/if.py
+++ IF_Condition/if.py
@@ -1,9 +1,29 @@
 """I am practicing my IF conditions"""
 
-n = int(input("Enter a number: "))
-print(n)
+# n = int(input("Enter a number: "))
+# print(n)
+#
+# message = "Number is even" if not n % 2 == 0 else "Number is odd"
+# print(message)
 
-if n % 2 == 0:
-    print(f"{n} is an even number")
+# n = 8
+#
+# if n > 10 or n % 2 == 0:
+#     print("Yay")
+# else:
+#     print("No")
+
+indian = ["samosa", "daal", "naan"]
+chinese = ["egg role", "pot sticker", "fried rice"]
+italian = ["pizza", "pasta", "risotto"]
+
+dish = input("Enter a dish: ").strip().lower()
+
+if dish in indian:
+    print(f"{dish} is indian")
+elif dish in chinese:
+    print(f" {dish} is chinese")
+elif dish in italian:
+    print(f"{dish} is italian")
 else:
-    print(f"{n} is an odd number")
+    print(f"Sorry, I don't know which cuisine {dish} belongs to.")
